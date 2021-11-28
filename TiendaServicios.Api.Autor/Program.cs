eee99962be31c5fd46333eae4c7147bd663341dc
Osman Mazariegos <omazariegosmz@gmail.com>
2021-11-28 08:07:56 -0600

    Arquitectura CQRS y Microservices

--- TiendaServicios.Api.Autor/Program.cs
+++ TiendaServicios.Api.Autor/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TiendaServicios.Api.Autor.Persistencia;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ContextoAutor>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ConexionDatabase"));
+}
+    );
 
 var app = builder.Build();
 
