88154d37acf18cea5f6baa84437ac87e1153100d
Osman Mazariegos <omazariegosmz@gmail.com>
2021-11-28 07:09:05 -0600

    Creacion de Microservice y arquitectura ASP.NET Core

---
+++ TiendaServicios.Api.Autor/Modelo/AutorLibro.cs
@@ -0,0 +1,15 @@
+﻿namespace TiendaServicios.Api.Autor.Modelo
+{
+    public class AutorLibro
+    {
+
+        public int AutorLibroId { get; set; }
+        public int Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public ICollection<GradoAcademico> ListaGradoAcademico { get; set; }
+        public string AutorLibroGuid { get; set; }
+
+
+    }
+}
