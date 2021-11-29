4fe98cf9828eb0d16fc687c9fd5ed5ca9ab98f87
Osman Mazariegos <omazariegosmz@gmail.com>
2021-11-29 14:46:57 -0600

    Arquitectura CQRS y Microservices

---
+++ TiendaServicios.Api.Autor/Aplicacion/Nuevo.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace TiendaServicios.Api.Autor.Aplicacion
+{
+    public class Nuevo
+    {
+        public class Ejecuta: IRequest
+        {
+
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public DateTime? FechaNacimiento { get; set; }
+
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            public Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    }
+}
