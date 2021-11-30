1a14ca142d147e3716e31bf04bc239a9d76c60da
Osman Mazariegos <omazariegosmz@gmail.com>
2021-11-30 09:44:48 -0600

    Consulta condicionar con Entity Framework Core

---
+++ TiendaServicios.Api.Autor/Aplicacion/ConsultaFiltro.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TiendaServicios.Api.Autor.Modelo;
+using TiendaServicios.Api.Autor.Persistencia;
+
+namespace TiendaServicios.Api.Autor.Aplicacion
+{
+    public class ConsultaFiltro
+    {
+
+        public class AutorUnico : IRequest<AutorLibro>
+        {
+            public string AutorGuid { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<AutorUnico, AutorLibro>
+        {
+            private readonly ContextoAutor _contexto;
+            public Manejador(ContextoAutor contexto)
+            {
+                _contexto = contexto;
+            }
+
+            public async Task<AutorLibro> Handle(AutorUnico request, CancellationToken cancellationToken)
+            {
+                var autor = await _contexto.AutorLibro.Where(x => x.AutorLibroGuid == request.AutorGuid).FirstOrDefaultAsync();
+
+                if (autor == null)
+                {
+                    throw new Exception("No se encontro el autor");
+                }
+
+                return autor;
+            }
+
+        }
+
+    }
+
+}
--- TiendaServicios.Api.Autor/Controllers/AutorController.cs
+++ TiendaServicios.Api.Autor/Controllers/AutorController.cs
@@ -31,6 +31,13 @@ namespace TiendaServicios.Api.Autor.Controllers
             return await _mediator.Send(new Consulta.ListaAutor());
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AutorLibro>> GetAutorLibro(string id)
+        {
+            return await _mediator.Send(new ConsultaFiltro.AutorUnico{ AutorGuid = id });
+        }
+
+
 
 
     }
