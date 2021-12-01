8d11a233c56c030628383bcb15bdea3945f29d4e
Osman Mazariegos <omazariegosmz@gmail.com>
2021-11-30 23:52:09 -0600

    Arquitectura de nuestra microservice Entity Framework Core y SQL Server en CQRS

---
+++ TiendaServicios.Api.Libros/Persistencia/ContextoLibreria.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TiendaServicios.Api.Libros.Modelo;
+
+namespace TiendaServicios.Api.Libros.Persistencia
+{
+    public class ContextoLibreria : DbContext
+    {
+        public ContextoLibreria(DbContextOptions<ContextoLibreria> options) : base(options) { }
+
+        public DbSet<LibreriaMaterial> LibreriaMaterial { get; set; }
+
+    }
+}
