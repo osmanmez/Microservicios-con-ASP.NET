564e47307b16fa6ef294bb04e6f079692fd8e619
Osman Mazariegos <omazariegosmz@gmail.com>
2021-12-01 20:40:50 -0600

    20. Code First - Migration con SQL Server,

---
+++ TiendaServicios.Api.Libro/Modelo/LibreriaMaterial.cs
@@ -0,0 +1,16 @@
+﻿namespace TiendaServicios.Api.Libros.Modelo
+{
+    public class LibreriaMaterial
+    {
+
+        public Guid? LibreriaMaterialId { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string? FechaPublicacion { get; set; }
+
+        public Guid AutorLibro { get; set; }  
+
+
+    }
+}
