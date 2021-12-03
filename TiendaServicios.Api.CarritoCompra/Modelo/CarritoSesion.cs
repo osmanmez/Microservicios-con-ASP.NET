913a9a08585e5a742c2af91a3d2c36f4a0d4fc1f
Osman Mazariegos <omazariegosmz@gmail.com>
2021-12-02 21:51:44 -0600

    Modelo del Carrito de Compras

---
+++ TiendaServicios.Api.CarritoCompra/Modelo/CarritoSesion.cs
@@ -0,0 +1,12 @@
+﻿namespace TiendaServicios.Api.CarritoCompra.Modelo
+{
+    public class CarritoSesion
+    {
+        public int CarritoSesionId { get;set;}
+
+        public string? FechaCreacion { get; set; }
+
+        public ICollection<CarritoSesionDetalle> ListaDetalle { get; set; }
+
+    }
+}
---
+++ TiendaServicios.Api.CarritoCompra/Modelo/CarritoSesionDetalle.cs
@@ -0,0 +1,16 @@
+﻿namespace TiendaServicios.Api.CarritoCompra.Modelo
+{
+    public class CarritoSesionDetalle
+    {
+        public int CarritoSesionDetalleId { get; set; }
+        public string? FechaCreacion { get; set; }
+
+        public string ProductoSeleccionado { get; set; }
+
+        public int CarritoSesionId { get; set; }
+
+        public CarritoSesion CarritoSesion {get; set;}
+
+
+    }
+}
