58ca83fb7fb48f4ca513e88155e79441322d9eb8
Osman Mazariegos <omazariegosmz@gmail.com>
2021-12-03 12:04:17 -0600

    Code First en MySql

---
+++ TiendaServicios.Api.CarritoCompra/Migrations/20211203180203_MigrationMysqlInicial.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiendaServicios.Api.CarritoCompra.Migrations
+{
+    public partial class MigrationMysqlInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CarritoSesion",
+                columns: table => new
+                {
+                    CarritoSesionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FechaCreacion = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarritoSesion", x => x.CarritoSesionId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CarritoSesionDetalle",
+                columns: table => new
+                {
+                    CarritoSesionDetalleId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FechaCreacion = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ProductoSeleccionado = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CarritoSesionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarritoSesionDetalle", x => x.CarritoSesionDetalleId);
+                    table.ForeignKey(
+                        name: "FK_CarritoSesionDetalle_CarritoSesion_CarritoSesionId",
+                        column: x => x.CarritoSesionId,
+                        principalTable: "CarritoSesion",
+                        principalColumn: "CarritoSesionId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarritoSesionDetalle_CarritoSesionId",
+                table: "CarritoSesionDetalle",
+                column: "CarritoSesionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarritoSesionDetalle");
+
+            migrationBuilder.DropTable(
+                name: "CarritoSesion");
+        }
+    }
+}
--- TiendaServicios.Api.CarritoCompra/Program.cs
+++ TiendaServicios.Api.CarritoCompra/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using TiendaServicios.Api.CarritoCompra.Persistencia;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CarritoContexto>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("ConexionDatabase");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+});
 
 var app = builder.Build();
 
