2b671fa57966b5cfc82e2edba1e262f9ec1709de
Osman Mazariegos <omazariegosmz@gmail.com>
2021-12-03 21:06:51 -0600

    Comunicacion entre Microservices

---
+++ TiendaServicios.Api.CarritoCompra/Aplicacion/CarritoDetalleDto.cs
@@ -0,0 +1,14 @@
+﻿namespace TiendaServicios.Api.CarritoCompra.Aplicacion
+{
+    public class CarritoDetalleDto
+    {
+        public Guid? LibroId  { get; set; }
+
+        public string TituloLibro { get; set; }
+        
+        public string AutorLibto { get; set; }
+
+        public DateTime? FechaPublicacion { get; set; }
+
+    }
+}
