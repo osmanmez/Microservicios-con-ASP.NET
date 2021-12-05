15fcf77ab32532c6b60329e8ee0c06c0ea906826
Osman Mazariegos <omazariegosmz@gmail.com>
2021-12-04 21:06:07 -0600

    Como implementar Unit Test en ASP.NET Core y Microservices?

---
+++ TiendaServicios.Api.Libro.Tests/LibrosServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TiendaServicios.Api.Libro.Tests
+{
+    public class LibrosServiceTest
+    {
+        [Fact]
+        public void GetLibros()
+        {
+
+        }
+
+    }
+}
