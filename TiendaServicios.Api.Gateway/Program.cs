d9cb05b3fb68e31e2d01576a41137bc0b7139061
Osman Mazariegos <omazariegosmz@gmail.com>
2021-12-09 21:02:04 -0600

    Creacion de Message Handler en el API Gateway

--- TiendaServicios.Api.Gateway/Program.cs
+++ TiendaServicios.Api.Gateway/Program.cs
@@ -1,13 +1,14 @@
 
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using TiendaServicios.Api.Gateway.MessageHandler;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 //builder.Services.AddControllers();
-builder.Services.AddOcelot();
+builder.Services.AddOcelot().AddDelegatingHandler<LibroHandler>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
