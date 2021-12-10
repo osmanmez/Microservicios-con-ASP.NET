d6e6fb077b27e52d6d08a695a81898998389f3cd
Osman Mazariegos <omazariegosmz@gmail.com>
2021-12-09 20:46:50 -0600

    Composition de Multiples Microservices  Request

---
+++ TiendaServicios.Api.Libro/Migrations/20211210021653_MigrationInicial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TiendaServicios.Api.Libro.Migrations
+{
+    public partial class MigrationInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AutorLibro",
+                table: "LibreriaMaterial",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AutorLibro",
+                table: "LibreriaMaterial",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
