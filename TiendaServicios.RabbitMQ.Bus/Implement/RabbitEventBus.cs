6fecff77d9c600e0c092c712a2119de62bf86dcd
Osman Mazariegos <omazariegosmz@gmail.com>
2021-12-10 21:59:10 -0600

    Implementacion  de service bus - parte 2

--- TiendaServicios.RabbitMQ.Bus/Implement/RabbitEventBus.cs
+++ TiendaServicios.RabbitMQ.Bus/Implement/RabbitEventBus.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -57,7 +58,38 @@ namespace TiendaServicios.RabbitMQ.Bus.Implement
             where T : Evento
             where TH : IEventoManejador<T>
         {
-            throw new NotImplementedException();
+            var eventoNombre = typeof(T).Name;
+            var manejadorEventoTipo = typeof(TH);
+
+            if (_eventoTipos.Contains(typeof(T)))
+            {
+                _eventoTipos.Add(typeof(T));
+            }
+
+            if (_manejadores.ContainsKey(eventoNombre))
+            {
+                _manejadores.Add(eventoNombre, new List<Type>());
+            }
+            if(_manejadores[eventoNombre].Any(x => x.GetType() == manejadorEventoTipo))
+            {
+                throw new ArgumentException($"El manejador {manejadorEventoTipo.Name} fue registrado anteriomente por {eventoNombre}");
+            }
+
+            _manejadores[eventoNombre].Add(manejadorEventoTipo);
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                DispatchConsumersAsync = true
+            };
+
+            var connection = factory.CreateConnection();
+                var channel = connection.CreateModel();
+            channel.QueueDeclare(eventoNombre, false, false, false, null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+           // consumer.Received += Consumer_Delegate;
+            channel.BasicConsume(eventoNombre, true, consumer);
         }
     }
 }
