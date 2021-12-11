500505c1f3c3e6360a582125d07683dbaa925512
Osman Mazariegos <omazariegosmz@gmail.com>
2021-12-11 15:38:11 -0600

    Creacion de proyecto libreria net core enviar email

---
+++ TiendaServicios.Mensajeria.Email/SendGridLibreria/Implement/SendGridEnviar.cs
@@ -0,0 +1,37 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaServicios.Mensajeria.Email.SendGridLibreria.Interface;
+using TiendaServicios.Mensajeria.Email.SendGridLibreria.Modelo;
+
+namespace TiendaServicios.Mensajeria.Email.SendGridLibreria.Implement
+{
+    public class SendGridEnviar : ISendGridEnviar
+    {
+        public async Task<(bool resultado, string errorMessage)> EnviarEmail(SendGridData data)
+        {
+            try
+            {
+
+                var sendGridCliente = new SendGridClient(data.SendGridAPIKey);
+                var destinatario = new EmailAddress(data.EmailDestinatario, data.NombreDestinatario);
+                var tituloEMail = data.Titulo;
+                var sender = new EmailAddress("osman.mazariegosmendez@gmail.com", "Osman Mazariegos");
+                var contenidoMensaje = data.Contenido;
+                var objMensaje = MailHelper.CreateSingleEmail(sender, destinatario, tituloEMail, contenidoMensaje, contenidoMensaje)
+                await sendGridCliente.SendEmailAsync(objMensaje);
+
+                return (true, null);
+
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+    }
+}
