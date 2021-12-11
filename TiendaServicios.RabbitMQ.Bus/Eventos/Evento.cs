4abde0bfbf395a801163c9a115f873b01755c1ab
Osman Mazariegos <omazariegosmz@gmail.com>
2021-12-10 21:01:30 -0600

    Interfaces Genericas para envio de mensajes

---
+++ TiendaServicios.RabbitMQ.Bus/Eventos/Evento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaServicios.RabbitMQ.Bus.Eventos
+{
+    public abstract class Evento
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Evento()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+    }
+}
